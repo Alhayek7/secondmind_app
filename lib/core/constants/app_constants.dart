2d6900c905972546dcc2933086bbe2de11dd332e
Ahmed alhayek <104521717+Alhayek7@users.noreply.github.com>
2026-06-11 03:48:04 +0300

    Delete lib/core/constants/app_constants.dart

--- lib/core/constants/app_constants.dart
+++
@@ -1,7 +0,0 @@
-﻿class AppConstants {
-  static const String appName = 'SecondMind';
-  static const String slogan = 'عقلك الثاني الذي لا ينسى';
-  static const String geminiApiKey =
-      'AIzaSyB1LrU-N-F4GHeMvix510EJ4-Q-ogu4ELI';
-  static const String geminiModel = 'gemini-2.5-flash';
-}
